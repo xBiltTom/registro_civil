c25085838483283e96af75c879cd5bf2d6bd15e1
Takeshy Velasquez Diaz <takeshydiaz05@gmail.com>
2025-06-12 02:03:29 -0500

    VistaActasNacimiento al 80 porciento

--- resources/views/livewire/actas/acta-nacimiento/create.blade.php
+++ resources/views/livewire/actas/acta-nacimiento/create.blade.php
@@ -1,4 +1,11 @@
 <div>
+    <div>
+        <a href="{{ route('actas-nacimiento') }}" wire:navigate>
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                Volver
+            </button>
+        </a>
+    </div>
     <div class="w-full p-6 bg-gray-100 dark:bg-gray-800 rounded-lg shadow-md">
         <form wire:submit.prevent="guardarNacimiento" class="space-y-6">
 
