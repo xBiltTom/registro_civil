cf3a044fa02cb5c776061ff3a682b244257b5476
Takeshy Velasquez Diaz <takeshydiaz05@gmail.com>
2025-07-10 02:12:39 -0500

    descripcion de cards en administracion

--- resources/views/livewire/administracion/admin-index.blade.php
+++ resources/views/livewire/administracion/admin-index.blade.php
@@ -22,12 +22,12 @@
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
                 <div class="w-full h-48">
-                    <img class="rounded-t-lg w-full h-full object-contain" src="{{asset('img/logoUsuario.png')}}" alt="" />
+                    <img class="rounded-t-lg w-full h-full object-contain" src="{{asset('img/logoUsuario.png')}}" alt="Imagen de usuarios" />
                 </div>
             </a>
             <div class="p-5">
                 <a href="#"><h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Los usuarios</h5></a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Gestiona las cuentas de usuario del sistema. Aquí puedes crear nuevos usuarios asociados a personas registradas y proporcionarles acceso al sistema de Registro Civil.</p>
                 <button type="button" x-on:click="$dispatch('open-modal')" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Crear usuario
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -39,12 +39,12 @@
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
                 <div class="w-full h-48">
-                    <img class="rounded-t-lg w-full h-full object-contain" src="{{asset('img/logoRol.png')}}" alt="" />
+                    <img class="rounded-t-lg w-full h-full object-contain" src="{{asset('img/logoRol.png')}}" alt="Imagen de roles" />
                 </div>
             </a>
             <div class="p-5">
                 <a href="#"><h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Los roles</h5></a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Administra los niveles de acceso y permisos en el sistema. Asigna roles específicos a los usuarios para definir qué funcionalidades pueden utilizar dentro del Registro Civil.</p>
                 <button type="button" x-on:click="$dispatch('open-assign-roles-modal')" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Asignar roles
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
